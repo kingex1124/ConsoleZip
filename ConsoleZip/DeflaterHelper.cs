7fb02d6ebe01d8e832703254d8a5e94ef09fc205
kevan.chen <kevan.chen@sinopa.com>
2020-10-05 14:59:40 +0800

    change

---
+++ ConsoleZip/DeflaterHelper.cs
@@ -0,0 +1,45 @@
+﻿using ICSharpCode.SharpZipLib.Zip.Compression;
+using ICSharpCode.SharpZipLib.Zip.Compression.Streams;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleZip
+{
+    public class DeflaterHelper
+    {
+        public static byte[] Compress(byte[] pBytes)
+        {
+            MemoryStream mMemory = new MemoryStream();
+            Deflater mDeflater = new Deflater(Deflater.BEST_COMPRESSION);
+            using (DeflaterOutputStream mStream = new DeflaterOutputStream(mMemory, mDeflater, 131072))
+            {
+                mStream.Write(pBytes, 0, pBytes.Length);
+            }
+
+            return mMemory.ToArray();
+        }
+
+        public static byte[] DeCompress(byte[] pBytes)
+        {
+            MemoryStream mMemory = new MemoryStream();
+            using (InflaterInputStream mStream = new InflaterInputStream(new MemoryStream(pBytes)))
+            {
+                Int32 mSize;
+                byte[] mWriteData = new byte[4096];
+                while (true)
+                {
+                    mSize = mStream.Read(mWriteData, 0, mWriteData.Length);
+                    if (mSize > 0)
+                        mMemory.Write(mWriteData, 0, mSize);
+                    else
+                        break;
+                }
+            }
+            return mMemory.ToArray();
+        }
+    }
+}
--- ConsoleZip/Program.cs
+++ ConsoleZip/Program.cs
@@ -14,9 +14,10 @@ namespace ConsoleZip
     {
         static void Main(string[] args)
         {
-            //DotNetZipHelper.ZipSingleFile(@"D:\SW_DVD9_NTRL_Visual_Studio_Pro_2015_ChnTrad_FPP_VL_MLF_X20-29933.ISO", @"D:\123.zip");
+            //DotNetZipHelper.ZipSingleFile(@"D:\hana\dpagent_windows.zip", @"D:\123.zip");
 
-            //var result = DotNetZipHelper.ZipSingleFileStream(@"D:\hana\dpagent_windows.zip");
+            var result = DotNetZipHelper.ZipSingleFileStream(@"D:\hana\dpagent_windows.zip");
+
         }
 
     }
